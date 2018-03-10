baca6207848628ca290cca20130fb48860eb837f
Jay Bednar <ironviralmedia@gmail.com>
2018-03-10 00:01:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,10 +35,10 @@ def select_series_title_with_most_human_characters
   JOIN books
   ON series.id = books.series_id
   JOIN character_books
-  ON books.id = character_books.book_id 
-  JOIN characters 
-  ON characters.id = character_books.character_id 
-  WHERE characters.species = 'human' 
+  ON books.id = character_books.book_id
+  JOIN characters
+  ON characters.id = character_books.character_id
+  WHERE characters.species = 'human'
   GROUP BY series.title
   ORDER BY COUNT(*)
   ;"
